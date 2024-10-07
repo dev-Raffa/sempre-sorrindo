635bb02804566f23e4b39cb28749d0fc1051012d
Rafael da Conceição <mnraffaell@gmail.com>
2024-10-07 19:04:39 -0300

    finish refact

--- src/app/components/slides/top/components.tsx
+++ src/app/components/slides/top/components.tsx
@@ -72,48 +72,5 @@ export const TopSlide = () => {
         </CarouselItem>
       </CarouselContent>
     </Carousel>
-    /*<>
-            <Swiper className="splide sess1__slide-desktop " >
-                <SwiperSlide>
-                    <Image src={image1} priority alt="duas pessoas sorrindo." className="object-cover" />
-                    <div className="banner__mobile-content">
-                        <h1>Com nossa tecnologia avançada, oferecemos implantes dentários <br/><span>em apenas 3 dias.</span>
-                        </h1>
-                        <a className="book__btn" href="https://api.whatsapp.com/send/?phone=5516981651441&text&type=phone_number&app_absent=0">Agende agora</a>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src={image2} loading="lazy" alt="selo do ranking great places to work" />
-                </SwiperSlide>
-            </Swiper>
-                {/*<div className="splide__track sess1__banner-track">
-                    <ul className="splide__list banner__list">
-                        <li className="splide__slide banner__list-item">
-
-                        </li>
-                        <li className="splide__slide banner__list-item">
-                            <Image src={image2} loading="lazy" alt="selo do ranking great places to work" />
-                        </li>
-                    </ul>
-                </div>
-            </Splide>
-            <div className="splide sess1__slide-desktop hidden lg:block" data-home="slide-desktop">
-                <div className="splide__track sess1__banner-track">
-                    <ul className="splide__list banner__list">
-                        <li className="splide__slide banner__list-item">
-                            <Image src={image1} loading="lazy" alt="duas pessoas sorrindo."/>
-                            <div className="banner__mobile-content">
-                            <h1>Com nossa tecnologia avançada, oferecemos implantes dentários <br/><span>em apenas 3 dias.</span>
-                            </h1>
-                            <a className="book__btn" href="https://api.whatsapp.com/send/?phone=5516981651441&text&type=phone_number&app_absent=0">Agende agora</a>
-                            </div>
-                        </li>
-                        <li className="splide__slide banner__list-item">
-                            <Image src={image2} loading="lazy" alt="selo do ranking great places to work"/>
-                        </li>
-                    </ul>
-                </div>
-            </div> */
-    //</>
   );
 };
